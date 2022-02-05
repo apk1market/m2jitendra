f679a26752280112915e245117d4fb9d28676771
Abir Hasan <hackitabirhasan@gmail.com>
2022-02-05 13:25:45 +0600

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -7,7 +7,7 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE, download_dict, download_dict_lock, Interval
 from bot.helper.ext_utils.bot_utils import get_readable_file_size, is_gdrive_link, is_gdtot_link, new_thread
-from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot, AppDrive
+from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot, AppDrive, HubDrive
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 
 
@@ -41,6 +41,14 @@ def cloneNode(update, context):
         try:
             msg = sendMessage(f"Processing: <code>{link}</code>", context.bot, update)
             link = AppDrive().appdrive_dl(link).get('gdrive_link')
+            deleteMessage(context.bot, msg)
+        except DirectDownloadLinkException as e:
+            deleteMessage(context.bot, msg)
+            return sendMessage(str(e), context.bot, update)
+    if "hubdrive.in" in link:
+        try:
+            msg = sendMessage(f"Processing: <code>{link}</code>", context.bot, update)
+            link = HubDrive().hubdrive_dl(link).get('gdrive_link')
             deleteMessage(context.bot, msg)
         except DirectDownloadLinkException as e:
             deleteMessage(context.bot, msg)
